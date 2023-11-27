e6f8d85af6c42ac27daa4519f38821348fbe59ae
aaronMulveyAI <aaron.mulvey.ai@gmail.com>
2023-11-27 19:42:25 +0100

    matrix arreglada

--- src/test/java/matrix/TestMatrix.java
+++ src/test/java/matrix/TestMatrix.java
@@ -1,6 +1,5 @@
 package matrix;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.nnet.matrix.Matrix;
 
@@ -10,7 +9,6 @@ public class TestMatrix {
     @Test
     public void matrixCreation() {
         Matrix matrix = new Matrix(3,4);
-        String text = matrix.toString();
         System.out.println(matrix);
     }
 }
